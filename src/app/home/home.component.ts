200b7bb1d092893bf59546ec793856427be34a92
pavankumarv <pavankumarv@wavecrest.gi>
2018-07-17 14:21:59 +0530

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+   styleUrls: ['./home.component.scss']
+   
+})
+export class HomeComponent implements OnInit {
+
+	btnText: string	= 'Add an Item'; 
+	goalText: string	=	"My First Life goal";
+
+    goals = [];
+	//goals = ['My first life goal', 'I want to climb a mountain', 'Go ice skiing'];
+	itemCount: number=this.goals.length;
+
+
+
+  	constructor(private _data: DataService) { }
+
+  	ngOnInit() { 
+
+  		this.itemCount	=	this.goals.length;
+  		this._data.goal.subscribe(res=>this.goals=res);
+  		this._data.changeGoal(this.goals);
+  	}
+
+  	addItem(){
+		this.goals.push(this.goalText);
+		this.goalText='';
+		this.itemCount	=	this.goals.length;
+		this._data.changeGoal(this.goals);
+  	}
+
+  	 removeItem(i) {
+	    this.goals.splice(i, 1);
+	    this._data.changeGoal(this.goals);
+	  }
+
+}
